359943ed34fb9c4e83ffb2be2f004f7c3fb84d3d
Limemani <ivan.horror2018@yandex.ru>
2020-09-08 16:40:30 +0300

    Begin

---
+++ PalendromFilter.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int UnicPalindrome(vector <string> jp) {
+	set <string> PO;
+	for (auto p : jp) {
+		PO.insert(p);
+	}
+	return PO.size();
+}
+
+bool IsPalindrome(string word) {
+	string temp = word;
+	for (int i = 0; i < word.size(); i++) {
+		temp[word.size() - i - 1] = word[i];
+	}
+	if (temp == word) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+vector <string> SplitInToWords(string text) {
+	vector <string> words;
+	string word;
+	for (int i = 0; i < text.size(); i++) {
+		if (text[i] == ' ') {
+			words.push_back(word);
+			word = ""s;
+		}
+		else {
+			word += text[i];
+		}
+	}
+	words.push_back(word);
+	return words;
+}
+
+vector <string> PalFilter(vector <string> words, int min_len) {
+	vector <string> res;
+	for (auto p : words) {
+		if (p.size() >= min_len) {
+			if (IsPalindrome(p)) {
+				res.push_back(p);
+			}
+		}
+	}
+	return res;
+}
+
+void enter(vector <string> vec) {
+	for (auto p : vec) {
+		cout << p << endl;
+	}
+}
+
+int main() {
+	string text;
+	vector <string> many_words;
+	getline(cin, text);
+	many_words = SplitInToWords(text);
+	many_words = PalFilter(many_words, 4);
+	cout << UnicPalindrome(many_words);
+}
